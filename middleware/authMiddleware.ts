48a649a07c7214622895433587fe95f516eba770
Sakshamebdev873 <vinodarya344@gmail.com>
2025-09-04 12:22:48 +0530

    feat: add auth middleware for token verification and user identification; update types for request object

---
+++ middleware/authMiddleware.ts
@@ -0,0 +1,28 @@
+import type { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { PrismaClient } from "../src/generated/prisma/client.js";
+const prisma = new PrismaClient();
+export const authMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const header = req.headers["authorization"];
+  if (!header) {
+    return res.status(401).json({ msg: "No token provided" });
+  }
+  const token = header.split(" ")[1] as string ;
+  try {
+    const decoded: any = jwt.verify(token, process.env.JWT_SECRET as string);
+    const user = await prisma.user.findUnique({
+      where: { id: decoded.userId },
+    });
+    if (!user || user.accessToken !== token) {
+      return res.status(401).json({ msg: "Token expired or invalid" });
+    }
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    return res.status(401).json({ msg: "Invalid Token" });
+  }
+};
