e74c1178cb0b54b2bfe613b557b8633b29e653f9
Sakshamebdev873 <vinodarya344@gmail.com>
2025-09-05 23:25:54 +0530

    refactor: restructure server initialization and enhance Kafka message consumption

--- server.ts
+++ server.ts
@@ -1,23 +1,38 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import authRouter from './routes/authRoutes.js'
-import chatRouter from './routes/chatRoutes.js'
+import authRouter from "./routes/authRoutes.js";
+import http from "http";
+import chatRouter from "./routes/chatRoutes.js";
 import { authMiddleware } from "./middleware/authMiddleware.js";
+import { initSocket } from "./libs/socket.js";
+import { consumer, initKafka } from "./libs/kafka.js";
+import { publisher } from "./libs/redis.js";
 const app = express();
 const port = process.env.PORT || 5101;
 
 app.use(cors());
 app.use(bodyParser.json());
 // router
-app.use('/api/v1',authRouter)
-app.use('/api/v1',authMiddleware,chatRouter)
-
+app.use("/api/v1", authRouter);
+app.use("/api/v1", authMiddleware, chatRouter);
+const server = http.createServer(app);
+const io = initSocket(server);
+(async () => {
+  await initKafka();
+  await consumer.subscribe({ topic: "chat-messages" });
+  await consumer.run({
+    eachMessage: async ({ message }) => {
+      const data = JSON.parse(message.value!.toString());
+      console.log("Consumed from Kafka", data);
+      await publisher.publish("chat-channel", JSON.stringify(data));
+    },
+  });
+})();
 const start = async () => {
   try {
-    app.listen(port,()=>{
+    server.listen(port, () => {
       console.log(`Server is listening on port ${port}....`);
-      
     });
   } catch (error: any) {
     console.log(error);
